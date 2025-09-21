49cf88287c07ed1365a08f0b038a8077b68a1f15
princesseulla <phaducaprincesseulla@gmail.com>
2025-09-21 10:12:51 +0800

    Add files via upload

---
+++ CS2B_MTExam_HADUCA/js/script.js
@@ -0,0 +1,129 @@
+//this is my js buttons that connect into the add and remove to cart buttons
+//to make it function the buttons.
+
+document.getElementById('button1').addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD!");
+}
+document.getElementById('b1').addEventListener('click', buttons);
+
+function buttons(){
+    alert("SUCCESSFULLY REMOVED!");
+}
+
+document.getElementById('button2').addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD!");
+}
+document.getElementById('b2').addEventListener('click', buttons);
+
+function buttons(){
+    alert("SUCCESSFULLY REMOVED!");
+}
+
+document.getElementById('button3').addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD!");
+}
+document.getElementById('b3').addEventListener('click', buttons);
+
+function buttons(){
+    alert("SUCCESSFULLY REMOVED!");
+}
+document.getElementById('button4').addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD!");
+}
+document.getElementById('b4').addEventListener('click', buttons);
+
+function buttons(){
+    alert("SUCCESSFULLY REMOVED!");
+}
+document.getElementById('button5').addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD!");
+}
+document.getElementById('b5').addEventListener('click', buttons);
+
+function buttons(){
+    alert("SUCCESSFULLY REMOVED!");
+}
+document.getElementById('button6').addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD!");
+}
+document.getElementById('b6').addEventListener('click', buttons);
+
+function buttons(){
+    alert("SUCCESSFULLY REMOVED!");
+}
+document.getElementById('button7').addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD!");
+}
+document.getElementById('b7').addEventListener('click', buttons);
+
+function buttons(){
+    alert("SUCCESSFULLY REMOVED!");
+}
+document.getElementById('button8').addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD!");
+}
+document.getElementById('b8').addEventListener('click', buttons);
+
+function buttons(){
+    alert("SUCCESSFULLY REMOVED");
+}
+
+document.getElementById('button9').addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD!");
+}
+document.getElementById('b9').addEventListener('click', buttons);
+
+function buttons(){
+    alert("SUCCESSFULLY REMOVED!");
+}
+document.getElementById('button10').addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD!");
+}
+document.getElementById('b10').addEventListener('click', buttons);
+
+function buttons(){
+    alert("SUCCESSFULLY REMOVED!");
+}
+document.getElementById('button11').addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD!");
+}
+document.getElementById('b11').addEventListener('click', buttons);
+
+function buttons(){
+    alert("SUCCESSFULLY REMOVED!");
+}
+document.getElementById('button12').addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD!");
+}
+document.getElementById('b12').addEventListener('click', buttons);
+
+function buttons(){
+    alert("SUCCESSFULLY REMOVED!");
+}
+
+
+
