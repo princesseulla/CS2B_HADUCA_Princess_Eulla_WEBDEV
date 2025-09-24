df3dc123bd35e393d1e14be2e9402e15f0513336
princesseulla <phaducaprincesseulla@gmail.com>
2025-09-24 21:14:24 +0800

    Add files via upload

---
+++ CS2B_MCO_HADUCA_P/js/script.js
@@ -0,0 +1,65 @@
+document.getElementById("b1").addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD TO YOUR CART!");
+}
+document.getElementById("b2").addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD TO YOUR CART!");
+}
+document.getElementById("b3").addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD TO YOUR CART!");
+}
+document.getElementById("b4").addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD TO YOUR CART!");
+}
+document.getElementById("b5").addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD TO YOUR CART!");
+}
+document.getElementById("b6").addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD TO YOUR CART!");
+}
+document.getElementById("b7").addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD TO YOUR CART!");
+}
+document.getElementById("b8").addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD TO YOUR CART!");
+}
+document.getElementById("b9").addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD TO YOUR CART!");
+}
+document.getElementById("b10").addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD TO YOUR CART!");
+}
+document.getElementById("b11").addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD TO YOUR CART!");
+}
+document.getElementById("b12").addEventListener('click', button);
+
+function button(){
+    alert("SUCCESSFULLY ADD TO YOUR CART!");
+}
+document.getElementById("send").addEventListener('click', buttonsend);
+
+function buttonsend(){
+    alert("THANK YOU!!");
+}
